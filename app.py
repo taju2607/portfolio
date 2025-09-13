7cf1f4fb37a6d72d3f850927b42ce8dc98f76a2b
taju2607 <103894413+taju2607@users.noreply.github.com>
2025-09-13 14:48:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def send_message():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-        app.run(debug=True, address='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
 
